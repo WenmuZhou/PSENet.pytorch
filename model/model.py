83441de6ad37ad3604c398aafba0633f53e7e5cc
zhoujun <572459439@qq.com>
2019-06-11 11:11:02 +0800

    add resnet18,34 backbone

--- model/model.py
+++ model/model.py
@@ -4,9 +4,18 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from model.resnet import *
+from models.resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from models.mobilenetv3 import MobileNetV3_Large, MobileNetV3_Small
+from models.ShuffleNetV2 import shufflenet_v2_x1_0
 
-d = ['resnet50', 'resnet101', 'resnet152']
+d = {'resnet18': {'model': resnet18, 'out': [64, 128, 256, 512]},
+     'resnet34': {'model': resnet34, 'out': [64, 128, 256, 512]},
+     'resnet50': {'model': resnet50, 'out': [256, 512, 1024, 2048]},
+     'resnet101': {'model': resnet101, 'out': [256, 512, 1024, 2048]},
+     'resnet152': {'model': resnet152, 'out': [256, 512, 1024, 2048]},
+     'MobileNetV3_Large': {'model': MobileNetV3_Large, 'out': [24, 40, 160, 160]},
+     'MobileNetV3_Small': {'model': MobileNetV3_Small, 'out': [16, 24, 48, 96]},
+     'shufflenetv2': {'model': shufflenet_v2_x1_0, 'out': [24, 116, 232, 464]}}
 inplace = True
 
 
@@ -15,31 +24,32 @@ class PSENet(nn.Module):
         super(PSENet, self).__init__()
         assert backbone in d, 'backbone must in: {}'.format(d)
         self.scale = scale
-        self.backbone = globals()[backbone](pretrained=pretrained)
+        conv_out = 256
+        model, out = d[backbone]['model'], d[backbone]['out']
+        self.backbone = model(pretrained=pretrained)
 
         # Top layer
-        self.toplayer = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)  # Reduce channels
+        self.toplayer = nn.Conv2d(out[3], conv_out, kernel_size=1, stride=1, padding=0)  # Reduce channels
+        # Lateral layers
+        self.latlayer1 = nn.Conv2d(out[2], conv_out, kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d(out[1], conv_out, kernel_size=1, stride=1, padding=0)
+        self.latlayer3 = nn.Conv2d(out[0], conv_out, kernel_size=1, stride=1, padding=0)
 
         # Smooth layers
-        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-
-        # Lateral layers
-        self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer2 = nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer3 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
+        self.smooth1 = nn.Conv2d(conv_out, conv_out, kernel_size=3, stride=1, padding=1)
+        self.smooth2 = nn.Conv2d(conv_out, conv_out, kernel_size=3, stride=1, padding=1)
+        self.smooth3 = nn.Conv2d(conv_out, conv_out, kernel_size=3, stride=1, padding=1)
 
         self.conv = nn.Sequential(
-            nn.Conv2d(1024, 256, kernel_size=3, padding=1, stride=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(conv_out * 4, conv_out, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(conv_out),
             nn.ReLU(inplace=inplace)
         )
-        self.out_conv = nn.Conv2d(256, result_num, kernel_size=1, stride=1)
+        self.out_conv = nn.Conv2d(conv_out, result_num, kernel_size=1, stride=1)
 
     def forward(self, input: torch.Tensor):
         _, _, H, W = input.size()
-        c1, c2, c3, c4, c5 = self.backbone(input)
+        c2, c3, c4, c5 = self.backbone(input)
         # Top-down
         p5 = self.toplayer(c5)
         p4 = self._upsample_add(p5, self.latlayer1(c4))
@@ -55,23 +65,29 @@ class PSENet(nn.Module):
         x = self.out_conv(x)
 
         x = F.interpolate(x, size=(H // self.scale, W // self.scale), mode='bilinear', align_corners=True)
-        # x = torch.sigmoid(x)
         return x
 
     def _upsample_add(self, x, y):
-        return F.interpolate(x, size=y.size()[2:], mode='bilinear', align_corners=True) + y
+        return F.interpolate(x, size=y.size()[2:], mode='bilinear', align_corners=False) + y
 
     def _upsample_cat(self, p2, p3, p4, p5):
         h, w = p2.size()[2:]
-        p3 = F.interpolate(p3, size=(h, w), mode='bilinear', align_corners=True)
-        p4 = F.interpolate(p4, size=(h, w), mode='bilinear', align_corners=True)
-        p5 = F.interpolate(p5, size=(h, w), mode='bilinear', align_corners=True)
+        p3 = F.interpolate(p3, size=(h, w), mode='bilinear', align_corners=False)
+        p4 = F.interpolate(p4, size=(h, w), mode='bilinear', align_corners=False)
+        p5 = F.interpolate(p5, size=(h, w), mode='bilinear', align_corners=False)
         return torch.cat([p2, p3, p4, p5], dim=1)
 
 
 if __name__ == '__main__':
-    device = torch.device('cuda:3')
-    net = PSENet(backbone='resnet152', pretrained=False, result_num=8).to(device)
-    x = torch.zeros(1, 3, 640, 640).to(device)
+    import time
+
+    device = torch.device('cpu')
+    backbone = 'shufflenetv2'
+    net = PSENet(backbone=backbone, pretrained=False, result_num=1).to(device)
+    net.eval()
+    x = torch.zeros(1, 3, 512, 512).to(device)
+    start = time.time()
     y = net(x)
+    print(time.time() - start)
     print(y.shape)
+    # torch.save(net.state_dict(),f'{backbone}.pth')
